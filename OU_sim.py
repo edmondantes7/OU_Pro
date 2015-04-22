ae6c610eca04172c75413a5b0d43d300a8142bdc
edantes <richdewey@gmail.com>
2015-04-21 20:33:13 -0700

    updated script

--- OU_sim.py
+++ OU_sim.py
@@ -1,2 +1,29 @@
-# This code will simulate a basic OU Process
-# Richard Dewey March 29th 2015
+# This will simualte random paths using an OU Process
+
+import numpy as np
+import pandas as pd 
+from math import exp
+from math import sqrt
+randn = np.random.randn
+
+
+head=list(range(0,100))
+rows=list(range(0,100))
+
+
+
+path1 = pd.DataFrame(np.zeros((100,100)),index=head, columns=rows)
+
+
+path1[0,1] = 1
+delta1 = .25
+lambda1 = .4
+mu1 = .03
+sig1 = .30
+
+
+for x in range(0,90):
+	path1.loc[x+1,0] = path1.loc[x,0]*exp(-lambda1*delta1) + mu1*(1-exp(-lambda1*delta1)) + (sig1*sqrt((1-exp(-2*lambda1*delta1))/(2*lambda1)))*randn()
+
+
+path1.to_csv('path1_test.csv')
