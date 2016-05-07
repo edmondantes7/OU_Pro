9d6de3b3f395fa904a93a0fa2fec89da809bc91c
Richard Dewey <rld2126@columbia.edu>
2016-05-07 11:02:26 -0700

    test change

--- OU_sim.py
+++ OU_sim.py
@@ -1,5 +1,7 @@
 # This will simualte random paths using an OU Process
 
+# This is a new file that has been created on 5/7/2016
+
 import numpy as np
 import pandas as pd 
 from math import exp
@@ -24,7 +26,6 @@ sig1 = .30
 
 for x in range(0,90):
 	path1.loc[x+1,0] = path1.loc[x,0]*exp(-lambda1*delta1) + mu1*(1-exp(-lambda1*delta1)) + (sig1*sqrt((1-exp(-2*lambda1*delta1))/(2*lambda1)))*randn()
-
 
 path1.to_csv('path1_test.csv')
 
